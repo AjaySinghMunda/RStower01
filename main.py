eec6c5b8db546bfe68258736fe63e36afa121211
Ajay <132570081+Ajayjsb@users.noreply.github.com>
2026-06-15 09:01:56 +0530

    remove bg

--- main.py
+++ main.py
@@ -2,16 +2,7 @@ import streamlit as st
 
 
 
-st.html("""
-    <style>
-        .stApp {
-            background: #314755;  /* fallback for old browsers */
-            background: -webkit-linear-gradient(to right, #26a0da, #314755);  /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to right, #26a0da, #314755); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-        }
-    </style>
-""")
 
 
 st.logo(image="code.png", size="large")
